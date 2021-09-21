1b01cf8e2e7bfaa9d813de3bfe5698f09bd147f3
Monishka <monishkagohil1194@gmail.com>
2021-09-21 16:48:28 +0200

    pushing the files from desktop

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ document.querySelector("#submit").addEventListener("click", () => {
       hexcode1 += hex_numbers[random_index];
     random_index = Math.floor(Math.random() * hex_numbers.length);
       hexcode2 += hex_numbers[random_index];
-  }
+  };
 
   document.body.style.background = `linear-gradient(to right, #${hexcode1}, #${hexcode2})`;
+
   document.querySelector("#hexcode1").textContent = hexcode1;
+  
   document.querySelector("#hexcode2").textContent = hexcode2;
 });
